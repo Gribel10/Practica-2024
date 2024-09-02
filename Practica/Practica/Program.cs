3aa6f4a58a04d3481a4235a4004d2c60a14a5394
Kelly Gribel Ayala Torres <ayalagribel39@gmail.com>
2024-09-01 20:41:46 -0600

    Suma de numeros

---
+++ Practica/Practica/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10 ;
+            int b = 15 ;
+            int c = 3 ;
+
+            int suma = a + b + c ;
+
+            Console.WriteLine("La suma es " + suma);
+            Console.ReadLine();
+        }
+    }
+}
